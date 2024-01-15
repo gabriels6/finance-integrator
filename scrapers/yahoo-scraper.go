6912db7aa80b0116e358a1c2837a8fbe49c0662c
Gabriel Gomes <gabrielsogom@gmail.com>
2024-01-15 10:22:02 -0300

    V1.6.11 Implementing csv download on yearly quote

--- scrapers/yahoo-scraper.go
+++ scrapers/yahoo-scraper.go
@@ -2,7 +2,6 @@ package scrapers
 
 import (
 	"github.com/gocolly/colly/v2"
-	"github.com/PuerkitoBio/goquery"
 	"strings"
 )
 
@@ -12,29 +11,29 @@ func YearlyQuotes(symbol string) []byte {
 	// Instantiate default collector
 	c := colly.NewCollector(
 		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("finance.yahoo.com"),
+		colly.AllowedDomains("query1.finance.yahoo.com"),
 	)
 
 	// Extracts asset value
-	c.OnHTML(`body`, func(e *colly.HTMLElement) {
-		goquerySelection := e.DOM
-
-		goquerySelection.Find(`table[data-test="historical-prices"] tbody tr`).Each (func(index int,item *goquery.Selection) {
-			indexItem := ""
-			
-			date := item.Find("td:nth-child(1)").Text()
-			value := item.Find("td:nth-child(5)").Text()
-
-			indexItem = indexItem + CreateJsonStringField("date",date, true)
-			indexItem = indexItem + CreateJsonStringField("value",value, false)
-
-			indexItem = "{"+indexItem+"},"
-
-			body = body + indexItem
-		})
+	c.OnResponse(func(r *colly.Response) {
+		lines := strings.Split(string(r.Body),"\n")
+		for idx, line := range lines {
+			if idx > 0 {
+				lineValues := strings.Split(line,",")
+				item := ""
+				item = item + CreateJsonStringField("date",string(lineValues[0]), true)
+				item = item + CreateJsonStringField("open",string(lineValues[1]), true)
+				item = item + CreateJsonStringField("high",string(lineValues[2]), true)
+				item = item + CreateJsonStringField("low",string(lineValues[3]), true)
+				item = item + CreateJsonStringField("close",string(lineValues[4]), true)
+				item = item + CreateJsonStringField("adjusted_close",string(lineValues[5]), true)
+				item = item + CreateJsonStringField("volume",string(lineValues[6]), false)
+				body += "{"+item+"},"
+			}
+		}
 	})
 
-	c.Visit("https://finance.yahoo.com/quote/"+symbol+"/history?frequency=1wk")
+	c.Visit("https://query1.finance.yahoo.com/v7/finance/download/"+symbol+"?period1=1673788703&period2=1705324703&interval=1wk&events=history&includeAdjustedClose=true")
 
 	if len(body) > 0 {
 		// Removes last comma
