342c5577ed1e26604ce093ce4464151e9bcc0cf7
Gabriel Gomes <gabrielsogom@gmail.com>
2025-05-05 21:00:26 -0300

    V1.9.1 Fixing bcb to get all rates of day

--- apis/bcb.go
+++ apis/bcb.go
@@ -3,7 +3,7 @@ package apis
 import "fmt"
 
 func GetBcbExchangeRate(currency string, date string) []byte {
-	return CallApi(fmt.Sprintf("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='%s'&@dataCotacao='%s'&$top=1&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao", currency, date))
+	return CallApi(fmt.Sprintf("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='%s'&@dataCotacao='%s'&$top=5&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao", currency, date))
 }
 
 func GetBcbExchangeRateByPeriod(currency string, startDate string, endDate string) []byte {
