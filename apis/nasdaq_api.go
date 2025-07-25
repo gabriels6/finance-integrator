5cac0fbe39e638580772d006b96591da0fe7ea2e
Gabriel Gomes <gabrielsogom@gmail.com>
2025-07-25 19:33:18 -0300

    V1.9.2 Implementing NASDAQ stock screener endpoint

--- apis/nasdaq_api.go
+++ apis/nasdaq_api.go
@@ -1,8 +1,34 @@
 package apis
 
-func StocksScreener() []byte {
-	headers := make(map[string]string)
-	headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36Preconnected origins in 'Network dependency tree' insightThe 'Network dependency tree' insight now shows you a list of used or unused preconnected origins and preconnect candidates, if any.Server response and redirection times in 'Document request latency' insightThe 'Document request latency' insight now shows you server response time and, if any, redirection time.Geolocation accuracy parameter in SensorsThe Sensors panel now lets you set accuracy in geolocation emulation, so you can test the handling of different levels of GPS accuracy."
+import (
+	"fmt"
 
-	return CallApiWithHeaders("https://api.nasdaq.com/api/screener/etf", headers)
+	"github.com/gocolly/colly/v2"
+)
+
+func StocksScreener() []byte {
+
+	body := ""
+
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("api.nasdaq.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36")
+		r.Headers.Set("Referer", "https://www.nasdaq.com/")
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		body = string(r.Body)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", string(r.Body), "\nError:", err)
+	})
+
+	c.Visit("https://api.nasdaq.com/api/screener/stocks?download=true")
+
+	return []byte(body)
 }
