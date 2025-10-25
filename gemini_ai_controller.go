86d7d7c14a4df76dbceaac95afc35370606825cf
Gabriel Gomes <gabrielsogom@gmail.com>
2025-10-25 14:18:23 -0300

    V1.6.10 Improving Gemini parser

--- gemini_ai_controller.go
+++ gemini_ai_controller.go
@@ -44,6 +44,7 @@ func ParsePDFHandler(c *gin.Context) {
 			genai.NewPartFromText("\n \n"),
 			genai.NewPartFromText("The PDF file is a portfolio. Extract it to a json as the following { \"broker\":\"Broker\" \"portfolioCode\": \"1234\", \"date\":\"2025-10-01\" \"financialAmount\": 123456.78, \"currency\":\"BRL\" \"positions\": [ { \"assetType\": \"Cash\", \"assetCode\": \"TEST123\", \"assetDescription\":\"The stock TEST1234\", \"currency\":\"USD\", \"quantity\": 100.00, \"price\": 10.00, \"financialAmount\": 1000.0, \"maturityDate\":\"2025-05-06\"  } ]  }. The broker code must be the broker which generated the file, on upper case, with _ instead of spaces between words and no special characters."),
 			genai.NewPartFromText("Also, for the asset type, divide in current: FIRF (Fundos renda fixa), FIC FIRF, FIA (Fundos acoes), FIC FIA, FIC FIRF, FIM (Fundos Multimercado), FII (Fundos Imobiliarios), OTHER_FUNDS, FIXED INCOME PUBLIC (Government bonds), FIXED INCOME PRIVATE (Corporate Bonds), ETF, STOCKS, REITS, CASH, FUTURES, OPTIONS, DERIVATIVES, OTHERS. Also, if the sum of positions.financialAmount - portfolio.financialAmount is not zero, add the difference to a new position of assetType OTHER, assetDescription \"assets or liabities\", price = 1, quantity = 1 and financialAmount = the difference"),
+			genai.NewPartFromText("Be careful. Some position lines are consolidating an assetType or sector, and also the quantity is not always called quantity. If you cannot find it, try to get the best match, but all the numbers must be in the file."),
 		},
 		Role: "user",
 	}
